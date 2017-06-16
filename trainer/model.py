145eb80700890ffaa077623c8f9c4fb653478065
Scott Spencer <notnotscott@gmail.com>
2017-06-16 16:04:02 -0400

    dims fix

--- trainer/model.py
+++ trainer/model.py
@@ -87,7 +87,7 @@ def _cnn_model_fn(features, labels, mode):
   pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
 
   # Dense Layer
-  pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
+  pool2_flat = tf.reshape(pool2, [-1, 26214400])
   dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
   dropout = tf.layers.dropout(
       inputs=dense, rate=0.4, training=(mode == learn.ModeKeys.TRAIN))
