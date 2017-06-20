51f008422659079d4823ce497e4c741860420c56
Scott Spencer <notnotscott@gmail.com>
2017-06-20 15:24:53 -0400

    ".tfrecords" to ".tfrecord" in task

--- trainer/task.py
+++ trainer/task.py
@@ -36,10 +36,10 @@ def generate_experiment_fn(data_dir,
     return Experiment(
       model.build_estimator(output_dir),
       train_input_fn=model.get_input_fn(
-          filename=os.path.join(data_dir, 'train.tfrecords'),
+          filename=os.path.join(data_dir, 'train.tfrecord'),
           batch_size=train_batch_size),
       eval_input_fn=model.get_input_fn(
-          filename=os.path.join(data_dir, 'test.tfrecords'),
+          filename=os.path.join(data_dir, 'test.tfrecord'),
           batch_size=eval_batch_size),
       export_strategies=[saved_model_export_utils.make_export_strategy(
           model.serving_input_fn,
